cb597305a4c5ec7165dca341c48d364e7acd1ba0
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-06-16 21:55:22 +0900

    シーン、カメラ、レンダラー追加

--- src/main.js
+++ src/main.js
@@ -4,24 +4,43 @@ import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
 
 import * as THREE from "three";
-console.log(THREE);
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+// シーン作成
+const scene = new THREE.Scene();
+console.log(scene);
 
-setupCounter(document.querySelector("#counter"));
+// カメラ追加
+const camera = new THREE.PerspectiveCamera(
+  50,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+console.log(camera);
+
+// レンダラー追加
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.render(scene, camera);
+
+// document.querySelector("#app").innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `;
+
+// setupCounter(document.querySelector("#counter"));
