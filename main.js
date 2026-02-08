8b7bdb643985ed967b82962ea6eb1d470ff93e35
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-08 13:57:03 +0900

    10

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -18,14 +19,16 @@ scene.add(cube);
 
 camera.position.z = 5;
 
+const controls = new OrbitControls(camera, renderer.domElement);
+
 // --- これまでの renderer.render(scene, camera); を削除し、
 //     アニメーションループを作成してください ---
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.03;
-  cube.rotation.y += 0.03;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
