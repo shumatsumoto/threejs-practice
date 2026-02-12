ecbf294047228d6e86d33a7b6ce41b965d3422b8
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-12 15:16:44 +0900

    12

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
-  1000
+  1000,
 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -20,6 +20,19 @@ scene.add(cube);
 camera.position.z = 5;
 
 // --- ここでライトを追加してください ---
+const ambientLight = new THREE.AmbientLight(0xffffff); // 環境光
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // 平行光源
+directionalLight.position.set(5, 5, 5).normalize();
+scene.add(directionalLight);
+
+// directionalLightのヘルパーを追加してライトの位置を可視化
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  1,
+);
+scene.add(directionalLightHelper);
 
 // --- ここまで ---
 
