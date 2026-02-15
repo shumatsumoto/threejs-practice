bda644614fbc2e9ea0c08118a8761e22b3391f0a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-15 18:07:20 +0900

    23

--- main.js
+++ main.js
@@ -18,30 +18,26 @@ scene.add(cube);
 
 camera.position.z = 5;
 
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-// マウス移動時に座標を更新
-window.addEventListener("mousemove", (event) => {
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+// --- ここでキーボードイベントを処理してください ---
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      cube.position.y += 0.1;
+      break;
+    case "ArrowDown":
+      cube.position.y -= 0.1;
+      break;
+    case "ArrowLeft":
+      cube.position.x -= 0.1;
+      break;
+    case "ArrowRight":
+      cube.position.x += 0.1;
+      break;
+  }
 });
 
 function animate() {
   requestAnimationFrame(animate);
-
-  // --- ここでRaycaster判定とスケール変更を行ってください ---
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
-  if (intersects.length > 0) {
-    intersects[0].object.scale.set(1.5, 1.5, 1.5); // スケールを大きくする
-  } else {
-    cube.scale.set(1, 1, 1); // 元のスケールに戻す
-  }
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
   renderer.render(scene, camera);
 }
 animate();
