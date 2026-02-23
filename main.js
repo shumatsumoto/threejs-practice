5c3b6bba24259d99f163fc150868473b95221edc
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-23 13:19:24 +0900

    38

--- main.js
+++ main.js
@@ -11,27 +11,26 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const loader = new THREE.TextureLoader();
-const texture = loader.load(
-  "https://threejs.org/examples/textures/terrain/grasslight-big.jpg"
-);
+// --- ここでスカイボックスを作成 ---
+const loader = new THREE.CubeTextureLoader();
+const path = "https://threejs.org/examples/textures/cube/Bridge2/";
+const urls = [
+  "posx.jpg",
+  "negx.jpg",
+  "posy.jpg",
+  "negy.jpg",
+  "posz.jpg",
+  "negz.jpg",
+];
+const texture = loader.load(urls.map(url => path + url));
+scene.background = texture; // スカイボックスを背景に設定
 
-// --- ここでテクスチャの繰り返し設定 ---
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(10, 10);
-
-// --- ここで地面を作成 ---
-const geometry = new THREE.PlaneGeometry(100, 100);
-const material = new THREE.MeshBasicMaterial({ map: texture });
-const plane = new THREE.Mesh(geometry, material);
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
-
-camera.position.set(0, 2, 100);
+camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
+  // カメラを回して確認
+  camera.rotation.y += 0.002;
   renderer.render(scene, camera);
 }
 animate();
