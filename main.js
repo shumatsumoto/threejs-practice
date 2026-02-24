0c52b2d5fe9fe4c6ae502df7d79b4bc75d8a73ad
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-24 14:11:28 +0900

    44

--- main.js
+++ main.js
@@ -11,31 +11,46 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// --- ここでジオメトリとパーティクルを作成 ---
-// const geometry = new THREE.BufferGeometry();
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material = new THREE.MeshNormalMaterial();
+
+// --- ここでInstancedMeshを作成 ---
 // const count = 1000;
-// const positions = new Float32Array(count * 3);
-// ...
+// const mesh = new THREE.InstancedMesh(geometry, material, count);
+// const dummy = new THREE.Object3D();
 
-const geometry = new THREE.BufferGeometry();
+// for (let i = 0; i < count; i++) {
+//     ...
+// }
+
 const count = 1000;
-const positions = new Float32Array(count * 3);
-for (let i = 0; i < count * 3; i++) {
-  positions[i] = (Math.random() - 0.5) * 10;
-}
-geometry.setAttribute("position", new THREE.BufferAttribute(positions, 3));
+const mesh = new THREE.InstancedMesh(geometry, material, count);
+const dummy = new THREE.Object3D();
 
-const material = new THREE.PointsMaterial({ color: 0xffffff, size: 0.1 });
-const points = new THREE.Points(geometry, material);
-scene.add(points);
+for (let i = 0; i < count; i++) {
+  dummy.position.set(
+    (Math.random() - 0.5) * 20,
+    (Math.random() - 0.5) * 20,
+    (Math.random() - 0.5) * 20
+  );
+  dummy.rotation.set(
+    Math.random() * Math.PI,
+    Math.random() * Math.PI,
+    Math.random() * Math.PI
+  );
+  dummy.updateMatrix();
+  mesh.setMatrixAt(i, dummy.matrix);
+}
+
+scene.add(mesh);
 
 camera.position.z = 15;
 
 function animate() {
+
+  mesh.rotation.y += 0.001;
+  mesh.rotation.x += 0.001;
   requestAnimationFrame(animate);
-  // 回転させると綺麗
-  points.rotation.x += 0.01;
-  points.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
 animate();
