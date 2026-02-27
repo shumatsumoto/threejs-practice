00814a2382185ea6e91a313f37947d89ac2988f1
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-02-27 16:35:50 +0900

    64

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+import { GlitchPass } from "three/examples/jsm/postprocessing/GlitchPass";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -12,26 +12,20 @@ const camera = new THREE.PerspectiveCamera(
 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-// トーンマッピング設定（ブルームと相性が良い）
-renderer.toneMapping = THREE.ReinhardToneMapping;
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // 明るい緑
-const cube = new THREE.Mesh(geometry, material);
+const cube = new THREE.Mesh(
+  new THREE.BoxGeometry(2, 2, 2),
+  new THREE.MeshNormalMaterial()
+);
 scene.add(cube);
 
-// --- ここでComposerとBloomPassを設定 ---
+// --- ComposerとGlitchPass ---
 const composer = new EffectComposer(renderer);
 composer.addPass(new RenderPass(scene, camera));
 
-const bloomPass = new UnrealBloomPass(
-  new THREE.Vector2(window.innerWidth, window.innerHeight),
-  2.5, // 強さを増加
-  0.8, // 半径を増加
-  0.5 // 閾値を低くして、より多くの部分がブルームする
-);
-composer.addPass(bloomPass);
+const glitchPass = new GlitchPass();
+composer.addPass(glitchPass); 
 
 camera.position.z = 5;
 
