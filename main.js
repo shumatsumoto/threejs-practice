aa63558ffb5455b6bd2e9c5e89d6f998488410e5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2026-03-01 13:52:43 +0900

    68

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
 import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
-import { AfterimagePass } from "three/examples/jsm/postprocessing/AfterimagePass";
+import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,30 +14,37 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(2, 2, 2),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshNormalMaterial()
 );
-scene.add(cube);
+cube1.position.x = -2;
+scene.add(cube1);
 
-// --- ComposerとAfterimagePass ---
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshNormalMaterial()
+);
+cube2.position.x = 2;
+scene.add(cube2);
+
+// --- ComposerとOutlinePass ---
 const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene, camera);
 composer.addPass(renderPass);
 
-const afterimagePass = new AfterimagePass();
-composer.addPass(afterimagePass);
+const outlinePass = new OutlinePass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  scene,
+  camera
+);
+outlinePass.selectedObjects = [cube1, cube2];
+composer.addPass(outlinePass);
 
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
-
-  // 高速回転
-  cube.rotation.x += 0.05;
-  cube.rotation.z += 0.05;
-  cube.position.x = Math.sin(Date.now() * 0.001) * 2;
-
   composer.render();
 }
 animate();
